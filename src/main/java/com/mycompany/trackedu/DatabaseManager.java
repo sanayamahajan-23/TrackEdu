e4e3651a6aa35ccbb30715232dbd71bfac493252
Divyaya22 <divyasehgal2004@gmail.com>
2024-11-07 22:59:34 +0530

    Merge branch 'main' of https://github.com/sanayamahajan-23/TrackEdu
    # Conflicts:
    #	src/main/java/com/mycompany/trackedu/home.form
    #	src/main/java/com/mycompany/trackedu/home.java

--- src/main/java/com/mycompany/trackedu/DatabaseManager.java
+++ src/main/java/com/mycompany/trackedu/DatabaseManager.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 public class DatabaseManager {
     private static final String URL = "jdbc:mysql://localhost:3306/attendance_db";
-    private static final String USER = "admin";
+    private static final String USER = "root";
     private static final String PASSWORD = "admin";
 
     private Connection connection;
